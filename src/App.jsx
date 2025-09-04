cb9bd4fbae74df7a2a7de9a63fb774081edf4f65
pablo-codes <112553944+pablo-codes@users.noreply.github.com>
2025-09-04 08:29:43 +0100

    should have just used cra

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/eny-consulting-frontend">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/health" element={<Health />} />
